204bc331a7cc50d32f5dd3b8b5dece0bb9a7767a
shrikant-aher <shrikantah@gmail.com>
2016-02-27 00:30:16 +0530

    my first app in node

---
+++ Vidzy/routes/videos.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var monk = require('monk');
+var db = monk('localhost:27017/vidzy');
+
+router.get('/', function(req, res, next) {
+
+    var collection = db.get('videos');
+    collection.find({}, function(err, videos){
+        if (err) throw err;
+      	res.json(videos);
+    });
+});
+
+module.exports = router;
